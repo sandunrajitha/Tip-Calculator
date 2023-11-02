3a6ebba133034760f05cfcea3a281265def4caea
Rajitha Liyanage <rajitha1911@gmail.com>
2023-11-02 16:33:07 +0900

    Observe tip input view

--- tip-calculator/view/TipInputView.swift
+++ tip-calculator/view/TipInputView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Combine
+import CombineCocoa
 
 class TipInputView: UIView {
 
@@ -17,16 +19,28 @@ class TipInputView: UIView {
 
     private lazy var tenPercentTipButton: UIButton = {
         let button = buildTipButton(tip: .tenPercent)
+        button.tapPublisher.flatMap({
+            Just(Tip.tenPercent)
+        }).assign(to: \.value, on: tipSubject)
+            .store(in: &cancellable)
         return button
     }()
 
     private lazy var fifteenPercentTipButton: UIButton = {
         let button = buildTipButton(tip: .fifteenPercent)
+        button.tapPublisher.flatMap({
+            Just(Tip.fifteenPercent)
+        }).assign(to: \.value, on: tipSubject)
+            .store(in: &cancellable)
         return button
     }()
 
     private lazy var twentyPercentTipButton: UIButton = {
         let button = buildTipButton(tip: .twentyPercent)
+        button.tapPublisher.flatMap({
+            Just(Tip.twentyPercent)
+        }).assign(to: \.value, on: tipSubject)
+            .store(in: &cancellable)
         return button
     }()
 
@@ -62,6 +76,12 @@ class TipInputView: UIView {
         stackView.distribution = .fillEqually
         return stackView
     }()
+
+    private let tipSubject = CurrentValueSubject<Tip, Never>(.none)
+    var valuePublisher: AnyPublisher<Tip, Never> {
+        return tipSubject.eraseToAnyPublisher()
+    }
+    private var cancellable = Set<AnyCancellable>()
 
     init() {
         super.init(frame: .zero)
