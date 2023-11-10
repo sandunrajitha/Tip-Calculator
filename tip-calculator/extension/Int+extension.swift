c6674afa88ca197390a1879a753e7abd877411b7
Rajitha Liyanage <rajitha1911@gmail.com>
2023-11-10 13:01:41 +0900

    Observe split input view

---
+++ tip-calculator/extension/Int+extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+extension.swift
+//  tip-calculator
+//
+//  Created by Rajitha Liyanage on 2023-11-09.
+//
+
+import Foundation
+
+extension Int {
+    var stringValue: String {
+        return String(self)
+    }
+}
