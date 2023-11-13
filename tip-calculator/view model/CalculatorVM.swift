baa528b02572e5dcf24f02a76e8cc0ce8dee89d0
Rajitha Liyanage <rajitha1911@gmail.com>
2023-11-13 12:11:52 +0900

    Compute result in View Model

--- tip-calculator/view model/CalculatorVM.swift
+++ tip-calculator/view model/CalculatorVM.swift
@@ -16,17 +16,43 @@ class CalculatorVM {
         let splitPublisher: AnyPublisher<Int, Never>
     }
 
+    private var cancellables = Set<AnyCancellable>()
+
     struct Output {
         let updateViewPublisher: AnyPublisher<Result, Never>
     }
 
     func transform(input: Input) -> Output {
-        let result = Result(
-            amountPerPerson: 500,
-            totalBill: 1000,
-            totalTip: 50)
 
-        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+        let updateViewPublisher = Publishers.CombineLatest3(
+            input.billPublisher,
+            input.tipPublisher,
+            input.splitPublisher).flatMap { [unowned self] (bill, tip, split) in
+                let totalTip = getTipAmount(bill: bill, tip: tip)
+                let totalBill = bill + totalTip
+                let amountPerPerson = totalBill / Double(split)
+                let result = Result(
+                    amountPerPerson: amountPerPerson,
+                    totalBill: totalBill,
+                    totalTip: totalTip)
+                return Just(result)
+            }.eraseToAnyPublisher()
+
+        return Output(updateViewPublisher: updateViewPublisher)
     }
 
+        private func getTipAmount(bill: Double, tip: Tip) -> Double {
+            switch tip {
+            case .none:
+                return 0
+            case .tenPercent:
+                return bill * 0.1
+            case .fifteenPercent:
+                return bill * 0.15
+            case .twentyPercent:
+                return bill * 0.2
+            case .custom(let value):
+                return Double(value)
+            }
+        }
 }
